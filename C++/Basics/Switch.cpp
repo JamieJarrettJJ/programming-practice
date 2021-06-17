30f47b124400cd91d0208cd7d2810fa0ba50eaef
Jamie <jamiejarrett2000@gmail.com>
2021-06-17 23:21:22 +0100

    Added Switch.cpp

---
+++ C++/Basics/Switch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int	main()
+{
+    int age = 21;
+
+    switch(age) {
+        case 16:
+            cout << "You can drive now" << endl;
+            break;
+        case 18:
+            cout << "You can drink now" << endl;
+            break;
+        case 21:
+            cout << "You can drink in America now" << endl;
+            break;
+        default:
+            cout << "Sorry you get nothing"
+    }
+}
