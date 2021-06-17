215698e0393d4d8c9e685bf3221b981c160eec91
Jamie <jamiejarrett2000@gmail.com>
2021-06-17 17:23:44 +0100

    Added Classes.cpp

---
+++ C++/Basics/Classes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class JJClass {
+    public:
+        void dudeWeed() {
+            cout << "Dude, weed." << endl;
+        }
+};
+
+int main()
+{
+    JJClass JJObject;
+    JJObject.dudeWeed();
+    return 0;
+}
