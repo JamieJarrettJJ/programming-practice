5c6b13d8079fc4452db1275852234d60fe1ffa58
Jamie <jamiejarrett2000@gmail.com>
2021-06-18 17:43:52 +0100

    Added Exceptions.cpp

---
+++ C++/Bucky's Basics/Exceptions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    try {
+        int momsAge = 50;
+        int sonsAge = 34;
+
+        if (sonsAge > momsAge) {
+            throw 99;
+        }
+    }catch(int x) {
+        cout << "son cannot be older than mother, ERROR NUMBER: " << x << endl;
+    }
+}
