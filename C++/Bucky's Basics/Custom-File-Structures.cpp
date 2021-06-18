18eff935361eab606dbaef9d07786b6504bc40e3
Jamie <jamiejarrett2000@gmail.com>
2021-06-18 18:18:47 +0100

    Added Custom-File-Structures.cpp Added players.txt

---
+++ C++/Bucky's Basics/Custom-File-Structures.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ofstream File("Files/players.txt");
+
+    cout << "Enter player ID, name and money" << endl;
+    cout << "Press Ctrl+Z to quit\n" << endl;
+
+    int idNumber;
+    string name;
+    double money;
+
+    while(cin >> idNumber >> name >> money) {
+        File << idNumber << ' ' << name << ' ' << money << endl;
+    }
+}
