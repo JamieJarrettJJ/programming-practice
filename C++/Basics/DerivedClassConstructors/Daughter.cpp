5ee1863e04cf271d09899654cac7204d4139f321
Jamie <jamiejarrett2000@gmail.com>
2021-06-18 13:17:49 +0100

    Added Daughter.cpp

---
+++ C++/Basics/DerivedClassConstructors/Daughter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Mother.h"
+#include "Daughter.h"
+
+using namespace std;
+
+Daughter::Daughter() {
+    cout << "I am the daughter constructor" << endl;
+}
+
+Daughter::~Daughter() {
+    cout << "daughter deconstructor!" << endl;
+}
---
+++ C++/Basics/DerivedClassConstructors/Daughter.h
@@ -0,0 +1,10 @@
+#ifndef DAUGHTER_H
+#define DAUGHTER_H
+
+class Daughter {
+    public:
+        Daughter();
+        ~Daughter();
+};
+
+#endif
