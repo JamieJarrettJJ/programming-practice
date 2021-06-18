a7834d44c3997e073f311d58742a877ad5103447
Jamie <jamiejarrett2000@gmail.com>
2021-06-18 13:17:27 +0100

    Added Derived-Class-Constructors.cpp

---
+++ C++/Basics/Derived-Class-Constructors.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "DerivedClassConstructors/Mother.cpp"
+#include "DerivedClassConstructors/Daughter.cpp"
+
+using namespace std;
+
+int main() {
+    //Mother ma;
+
+    Daughter dina;
+}
