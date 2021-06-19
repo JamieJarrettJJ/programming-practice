e2a4658ba06eca0b87c3f5531f3bcca94cb01d58
Jamie <jamiejarrett2000@gmail.com>
2021-06-19 11:45:41 +0100

    Added Borze.cpp

---
+++ C++/CF Practice Ladders/1300/4-Borze.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/problemset/problem/32/B
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string b; //Borze alphabet
+    string d; //Decoded
+
+    cin >> b;
+
+    for(int i=0;i<b.size();i++) {
+        if(b[i] == '-' && b[i+1] == '.') {
+            d.append("1");
+            i++;
+        }else if(b[i] == '-' && b[i+1] == '-') {
+            d.append("2");
+            i++;
+        }else {
+            d.append("0");
+        }
+    }
+
+    cout << d << endl;
+}
